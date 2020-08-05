0cb62018e0a85ff2d8da98d2c1a72f653bea4c44
Tarun Kumar <tarunkumar@Taruns-MacBook-Pro-2.local>
2020-08-05 23:39:35 +0530

    added transition in h2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithubAlt, faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
@@ -6,6 +6,26 @@ import { faGithubAlt, faFacebookF, faLinkedinIn } from '@fortawesome/free-brands
 
 function App() {
 
+  const [word, setWord]= useState("Tarun Bhukya");
+  const [num, setNum] = useState(0);
+  const words = ["Tarun Bhukya", "Software Developer", "FullStack Developer", "Android App Developer", "Rookie Devops", "Freelancer"];
+  const [classWord, setClassWord] = useState("item hide");
+ 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const index = num % words.length;
+      setClassWord("item")
+      setWord(words[index])
+      setNum(num => num + 1)
+      setTimeout(function () {
+        setClassWord("item hide")
+      }, 2500)
+      console.log(num)
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [num, words]);
+    
 
   return (
     <div>
@@ -16,10 +36,12 @@ function App() {
             <div className="col-md-7 col-sm-12 col-xs-12">
 
               <div className="home-content">
-                <strong>Hello !</strong>
-                <h1>I Am Tarun Bhukya</h1>
+                <div className="title-container">
+                  <h2>I Am</h2>
+                  <div className={classWord}><h2>{word}</h2></div>
+                </div>
                 <p>I'm a Software Developer with extensive experience on building FullStack Web Applications and Android Application.</p>
-                <ul class="banner_social_link">	
+                <ul className="banner_social_link">	
                 				
 									<li><a href="https://github.com/tarunbhukya"><FontAwesomeIcon icon={faGithubAlt} /></a></li>
                   <li><a href="https://www.facebook.com/bethtarun/"><FontAwesomeIcon icon={faFacebookF} /></a></li>
